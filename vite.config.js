92fef7570d6efe076d9e0156dbd4af14720195b4
Shuayb (Jet) <jethrofj12@gmail.com>
2025-08-13 22:33:46 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Vite config for GitHub Pages under repo: Trojan-HIV-2
-export default defineConfig({
-  plugins: [react()],
-  base: '/Trojan-HIV-2/', // ← must match repo name exactly
-})
-
